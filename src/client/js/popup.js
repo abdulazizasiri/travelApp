b25037744aff3e4d3184db30d3a30bb88c4697bd
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-12-12 09:30:31 +0300

    created the popup for the add city

--- src/client/js/popup.js
+++ src/client/js/popup.js
@@ -1,7 +1,33 @@
+let button = document.getElementById("add_City")
+let inputField = document.getElementById("name")
+    // button.disabled = false
+    // inputField.addEventListener("keydown", function(e) {
+    //     let input = ""
+    //     input.concat(e.key)
+    //     console.log("Keyboard " + input)
+    //     if (inputField.value === "") {
+    //         button.style.background = "red"
+    //         button.disabled = false
+    //     }
+    // })
+
+
 function showPopup(event, code) {
+
     if (code === 1) {
         console.log("Event " + event)
         document.getElementsByClassName("modal-bg")[0].classList.add("bg-active")
+        console.log("Button " + button.innerText)
+
+        if (button.disabled === true) {
+            console.log("Disabled")
+            button.addEventListener("mousedown", function() {
+                button.background = 'red'
+            })
+
+            // button.style.background = 
+        }
+
     }
     if (code === 2) {
         console.log("Close")
@@ -9,7 +35,17 @@ function showPopup(event, code) {
 
     }
 
+    if (code === 3) {
+        // let value = document.getElementById("modal-bg")
+        console.log("Clicked addd trips")
+    }
+
+
     // element.classList.toggle("active")
+}
+
+function changeAttributes() {
+    console.log("Hello")
 }
 
 
