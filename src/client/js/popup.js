8fd46c8d9e3ded4d4d77953eb7ebde9ba43c76de
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-12-12 10:10:04 +0300

    env with a key

--- src/client/js/popup.js
+++ src/client/js/popup.js
@@ -1,32 +1,24 @@
-let button = document.getElementById("add_City")
-let inputField = document.getElementById("name")
-    // button.disabled = false
-    // inputField.addEventListener("keydown", function(e) {
-    //     let input = ""
-    //     input.concat(e.key)
-    //     console.log("Keyboard " + input)
-    //     if (inputField.value === "") {
-    //         button.style.background = "red"
-    //         button.disabled = false
-    //     }
-    // })
+// button.disabled = false
+// inputField.addEventListener("keydown", function(e) {
+//     let input = ""
+//     input.concat(e.key)
+//     console.log("Keyboard " + input)
+//     if (inputField.value === "") {
+//         button.style.background = "red"
+//         button.disabled = false
+//     }
+// })
 
 
 function showPopup(event, code) {
+    let button = document.getElementById("add_City")
 
     if (code === 1) {
         console.log("Event " + event)
+
         document.getElementsByClassName("modal-bg")[0].classList.add("bg-active")
         console.log("Button " + button.innerText)
 
-        if (button.disabled === true) {
-            console.log("Disabled")
-            button.addEventListener("mousedown", function() {
-                button.background = 'red'
-            })
-
-            // button.style.background = 
-        }
 
     }
     if (code === 2) {
@@ -38,15 +30,24 @@ function showPopup(event, code) {
     if (code === 3) {
         // let value = document.getElementById("modal-bg")
         console.log("Clicked addd trips")
+        let inputField = document.getElementById("name")
+        let result = document.getElementById("result")
+        if (inputField.value === "") {
+            result.innerText = "You have to enter a city"
+            result.style.marginBottom = "5px"
+            result.style.color = "red"
+        }
+        console.log(inputField.value)
+
     }
 
 
     // element.classList.toggle("active")
 }
 
-function changeAttributes() {
-    console.log("Hello")
-}
+// function changeAttributes() {
+//     console.log("Hello")
+// }
 
 
 export { showPopup }
